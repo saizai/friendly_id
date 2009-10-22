ed462511d03846b816f118d4b3d203d1a6892428
Sai Emrys <github@saizai.com>
2009-10-22 03:49:24 -0700

    Add :scope option for non-sluggables. Requires scope to be a column name on the model. May return more records than expected iff you pass it more than one ID without a scope specified in find, but only one if you pass it only one id, even though there might be more than one without scope specified. Maybe someone can fix?

--- lib/friendly_id/non_sluggable_class_methods.rb
+++ lib/friendly_id/non_sluggable_class_methods.rb
@@ -7,7 +7,14 @@ module FriendlyId::NonSluggableClassMethods
   protected
 
   def find_one(id, options) #:nodoc:#
-    if id.is_a?(String) && result = send("find_by_#{ friendly_id_options[:column] }", id, options)
+    if id.is_a?(String)
+      has_scope = options.has_key? :scope
+      if has_scope # if it's not sluggable, we assume that the scope is a column name (and not something fancy like a method or association)
+        scope = options.delete(:scope)
+        result = send("find_by_#{ friendly_id_options[:column] }_and_#{friendly_id_options[:scope]}", id, scope, options)
+      else
+        result = send("find_by_#{ friendly_id_options[:column] }", id, options)
+      end
       result.send(:found_using_friendly_id=, true)
     else
       result = super id, options
@@ -16,14 +23,23 @@ module FriendlyId::NonSluggableClassMethods
   end
 
   def find_some(ids_and_names, options) #:nodoc:#
-
-    results = with_scope :find => options do
-      find :all, :conditions => ["#{quoted_table_name}.#{primary_key} IN (?) OR #{friendly_id_options[:column].to_s} IN (?)",
-        ids_and_names, ids_and_names]
+    scopable = friendly_id_options.has_key? :scope
+    has_scope = options.has_key? :scope
+    scoped_find_options = {}
+    if has_scope
+      scope = options.delete(:scope)
+      scoped_find_options[:conditions] = {friendly_id_options[:scope] => scope }
     end
-
+    
+    results = with_scope :find => options do
+      with_scope :find => scoped_find_options do
+        find :all, :conditions => ["#{quoted_table_name}.#{primary_key} IN (?) OR #{friendly_id_options[:column].to_s} IN (?)",
+          ids_and_names, ids_and_names]
+        end
+    end
+    
     expected = expected_size(ids_and_names, options)
-    if results.size != expected
+    if ((!scopable or has_scope) and results.size != expected) or (scopable and !has_scope and results.size < expected) # with scope unspecified there might be more
       raise ActiveRecord::RecordNotFound, "Couldn't find all #{ name.pluralize } with IDs (#{ ids_and_names * ', ' }) AND #{ sanitize_sql options[:conditions] } (found #{ results.size } results, but was looking for #{ expected })"
     end
 
@@ -31,5 +47,10 @@ module FriendlyId::NonSluggableClassMethods
 
     results
 
+  end
+
+  def validate_find_options(options) #:nodoc:#
+    options.assert_valid_keys([:conditions, :include, :joins, :limit, :offset,
+      :order, :select, :readonly, :group, :from, :lock, :having, :scope])
   end
 end
